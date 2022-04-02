ec244a13b54cbb37ffe982a967f89cad87a0da28
unknown <2303885822@qq.com>
2022-04-02 09:28:44 +0800

    submit

--- src/packages/main.ts
+++ src/packages/main.ts
@@ -3,6 +3,10 @@ import "./fonts/iconfont.css";
 import HButton from "../components/h-button";
 const components = [HButton];
 
+// 按需
+export { HButton };
+
+// 全局
 const hUI = {
   install: (app: App) => {
     components.forEach((component) => app.component(component.name, component));
