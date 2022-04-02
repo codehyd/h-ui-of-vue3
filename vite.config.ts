c13b72bbf9dfd5efcd20046b79b2a644878d0f62
unknown <2303885822@qq.com>
2022-04-02 09:23:52 +0800

    submit

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/packages/main.ts"),
       name: "h-ui",
-      fileName: (format) => `lib.${format}.js`,
+      fileName: (format) => `lib.${format}.ts`,
     },
     rollupOptions: {
       external: ["vue"],
